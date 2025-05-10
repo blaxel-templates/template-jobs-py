eb2cf803da2de354e292e80f91fde502d3a2fa6e
drappier-charles <cdrappier@blaxel.ai>
2025-05-09 17:00:01 -0700

    Fix argument syntax

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from blaxel.jobs import bl_job
 
-def my_job(name: str) -> None:
-    print(f"Hello, world {name}!")
+def my_job(lastname: str = None, firstname: str = None) -> None:
+    print(f"Hello, world {firstname} {lastname}!")
 
 bl_job.start(my_job)
