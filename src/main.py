d6903138c507f9083d6f2c9c807bd724e56862fd
drappier-charles <cdrappier@blaxel.ai>
2025-05-11 16:43:36 -0700

    Upgrade SDK, use logger

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 from blaxel.jobs import bl_job
+import logging
+
+logger = logging.getLogger(__name__)
 
 def my_job(lastname: str = None, firstname: str = None) -> None:
-    print(f"Hello, world {firstname} {lastname}!")
+    logger.info(f"Hello, world {firstname} {lastname}!")
 
 bl_job.start(my_job)
